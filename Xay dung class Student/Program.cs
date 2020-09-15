da239791e66d48e307a5816202bccfb7bef09899
trinhnam12345z <trinhnam123456789z@gmail.com>
2020-09-15 22:54:05 +0700

    Initial commit

---
+++ Xay dung class Student/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Xay_dung_class_Student
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student(1, "M. Công", "Hà Nội", 30);
+
+            Student student2 = new Student();
+            student2.SetId(2);
+            student2.SetName("Ph. Quang Khánh");
+            student2.SetAddress("Lạng Sơn");
+            student2.SetAge(25);
+
+            Console.WriteLine(student1.ToString());
+            Console.WriteLine(student2.ToString());
+            Console.Read();
+        }
+    }
+}
